549ee0bbe7c9cfe993e5e0112a3dc7dcc6bcad1c
royaboya <94084201+royaboya@users.noreply.github.com>
2022-03-26 09:59:20 -0400

    initial window setup

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+pygame.init()
+pygame.display.set_caption('Collision Testing')
+window = pygame.display.set_mode((900,900), 0, 32)
+clock = pygame.time.Clock()
+
+while True:
+    window.fill((255,255,255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
+    clock.tick(60)
