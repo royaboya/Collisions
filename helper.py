2c89f892e8de35504ae3816c775ec3a41425e121
royaboya <94084201+royaboya@users.noreply.github.com>
2022-03-26 20:49:26 -0400

    fixed floor

--- helper.py
+++ helper.py
@@ -4,13 +4,31 @@ def add_obj(space: pymunk.Space, x: int, y: int):
     obj = pymunk.Body(10, 50, body_type=pymunk.Body.DYNAMIC)
     obj.position = (x, y)
     obj_shape = pymunk.Circle(obj, 50)
-    
+
     space.add(obj, obj_shape)
     return obj_shape
 
-def draw_obj(objects: list, window: pygame.display):
+def draw_obj(objects: list, window: pygame.display, color: tuple):
     for object in objects:
         x = int(object.body.position.x)
         y = int(object.body.position.y)
-        pygame.draw.circle(window, (0,0,0), (x, y), 50)
-        
+        pygame.draw.circle(window, color, (x, y), 50)
+
+def create_floor(space: pymunk.Space, window: pygame.display, mouse_pos: tuple):
+    # TOFIX
+    floor_obj = pymunk.Body(10, 10, body_type=pymunk.Body.STATIC)
+    width = window.get_width()
+    height = window.get_height()
+    
+    mY = mouse_pos[1]
+    floor_shape = pymunk.Poly(floor_obj, vertices=[(0, mY), (width, mY), (0, height), (width, height)])
+    space.add(floor_obj, floor_shape)
+
+    return floor_shape
+
+def draw_floor(floors: list, window: pygame.display, color: tuple, c_mouse_pos: tuple):
+    for floor in floors:
+        width = window.get_width()
+        mouse_y = c_mouse_pos[1]
+        box = pygame.Rect((0,mouse_y), (width, mouse_y))
+        pygame.draw.rect(window, color, box)
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pygame, sys, pymunk
-from helper import add_obj, draw_obj
-
+import constants
+from helper import *
 
 
 class App:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Collision Testing")
-
+        self.running = True
         self.window = pygame.display.set_mode((900, 900), 0, 32)
+
         self.space = pymunk.Space()
         self.space.gravity = (0, 300)
         self.objects = []
-        self.running = True
+        self.floors = []  # 1 for now, maybe change to multiple lines
+
         self.clock = pygame.time.Clock()
+        self.floor_position = (0, 0)  # find better solution to first click default
 
     def main(self):
         while self.running:
@@ -21,35 +24,28 @@ class App:
                 if event.type == pygame.QUIT:
                     self.running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    x = pygame.mouse.get_pos()[0]
-                    y = pygame.mouse.get_pos()[1]
-                    self.objects.append(add_obj(self.space, x, y))
+                    if event.button == 1:
+                        x = pygame.mouse.get_pos()[0]
+                        y = pygame.mouse.get_pos()[1]
+                        self.objects.append(add_obj(self.space, x, y))
+                    elif event.button == 3:
+                        if len(self.floors) == 0:
+                            self.floor_position = pygame.mouse.get_pos()
+                            self.floors.append(
+                                create_floor(
+                                    self.space, self.window, self.floor_position
+                                )
+                            )
+
             pygame.display.update()
-            # self.window.fill(255,255,255)
             self.window.fill("white")
-            draw_obj(self.objects, self.window)
-            self.space.step(1/60)
+            draw_obj(self.objects, self.window, constants.BLACK)  # draw pymunk objects
+            draw_floor(self.floors, self.window, constants.RED, self.floor_position)
+            self.space.step(1 / 60)
             self.clock.tick(60)
-        
+
         pygame.quit()
         sys.exit()
-
-# def main():
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == pygame.MOUSEBUTTONDOWN:
-#                 x = pygame.mouse.get_pos()[0]
-#                 y = pygame.mouse.get_pos()[1]
-#                 objects.append(add_obj(space, x, y))
-
-#         pygame.display.update()
-#         window.fill((255, 255, 255))
-#         draw_obj(objects, window)
-#         space.step(1/60)
-#         clock.tick(60)
 
 
 if __name__ == "__main__":
